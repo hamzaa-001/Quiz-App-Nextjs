fec027ef9664bb59d278d914a37afe211e4472c7
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-06-05 10:26:48 +0500

    Quick Fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
               aria-describedby="helper-text-explanation"
               value={newNumberOfQuestion}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="10"
+              placeholder="Enter number of Questions..."
               required
               onChange={(e) => setNewNumberOfQuestion(e.target.value)}
             />
